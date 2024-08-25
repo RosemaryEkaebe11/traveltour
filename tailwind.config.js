9b9910025e8957bc80bc7b9b1a4f40e07ef76a43
RosemaryEkebe11 <rosemaryekebe11@gmail.com>
2024-08-25 11:54:29 +0100

    new commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'orange': '#D66214',
+
+      },
+      screens:{
+       
+
+        'sm': {'min': '640px'	},  
+        // @media (min-width: 640px) { ... }
+        'md': {'min': '768px'	},  
+        // @media (min-width: 768px) { ... }
+        'xlg': {'min': '992px'},
+        'lg': {'min': '1024px'},	
+        // @media (min-width: 1024px) { ... }
+        'xl': {'min': '1280px'},	
+        // @media (min-width: 1280px) { ... }
+        '2xl':{'min': '1536px'},	
+        // @media (min-width: 1536px) { ... }
+      }
+    },
+  },
+  plugins: [],
+}
